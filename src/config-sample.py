dab28cfe8c0d4c9eba16b504395d8e80794fa05d
David Winterbottom <david.winterbottom@gmail.com>
2009-08-19 00:40:55 +0100

    Removed config file

---
+++ src/config-sample.py
@@ -0,0 +1,15 @@
+# Repositories to analyse
+# This should take the form name : url
+repos = {
+}
+
+# User's linked repos
+user_repos = {
+}
+
+# SVN access credentials
+svn_username = None
+svn_password = None
+
+# Misc
+analysis_period_in_days = 7
